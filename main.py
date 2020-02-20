44bc7d33af2c287963d546bd77a6c8bfccfc96a2
Favian Hazman <favian.hazman@aux.go-jek.com>
2020-02-20 10:26:10 +0700

    [FAVIAN] clean up main

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from vroducer.vroducer import vroducer
+from vroducer.utils.utils import get_avro_schema
 from dotenv import load_dotenv
 import os
 import random # TESTING
 import datetime
-from vroducer.utils.utils import get_avro_schema
+from flask import Flask
+from flask import request
+
 
 load_dotenv()
 
@@ -16,11 +19,21 @@ avro_schema = get_avro_schema(SCHEMA_FOLDER_PATH,TOPIC_NAME)
 
 vr = vroducer(avro_schema, BOOTSTRAP_SERVERS,SCHEMA_REGISTRY_PATH)
 
-for i in range(100):
-    VALUE = {
-    "mahasiswa_npm" : str(random.randint(1600000, 1699999)),
-    "course_id" : str(random.randint(10, 50)),
-    "message_timestamp" : str(datetime.datetime.now())
-    }
-    vr.produce_message(TOPIC_NAME, VALUE)
+# init Flask
+app = Flask(__name__)
 
+@app.route('/ping')
+def ping_pong():
+    return 'pong'
+
+@app.route('/single', methods=['POST'])
+def post_single_message():
+    if request.method == 'POST':
+        msg = request.get_json()
+        msg['message_timestamp'] = str(datetime.datetime.now())
+        msg = dict(msg)
+        vr.produce_message(TOPIC_NAME, msg)
+    return "a"
+
+
+app.run(host='0.0.0.0', port=8080, debug=True)
